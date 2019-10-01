2f5f6c22cdbb87f71e6da1e365dfba2b7b44ef2f
Pranav Rajan <rajan.pranav320@gmail.com>
2019-09-30 19:23:48 -0600

    cleaned up some stuff

--- hw5/js/table.js
+++ hw5/js/table.js
@@ -103,11 +103,6 @@ class Table {
             return minValue;
         }
 
-        // figure out the number of wins and losses for each country 
-        console.log("Calculate the number of wins");
-        let numWins = findMax(this.teamData, 'Wins');
-
-        console.log(numWins);
         //Update Scale Domains
         console.log("Team Data");
         console.log(this.teamData)
@@ -132,45 +127,7 @@ class Table {
         console.log("Updating the goal color scale");
         this.goalColorScale = d3.scaleLinear()
                                 .domain([0, goalScaleDomainMax])
-                                .range(['#cb181d', '#034e7b']);
-        
-        
-   
-
-
-        //
-        //console.log(goalDomainMax);
-
-
-        // console.log("finding the max value for the scale");
-        // let goalsMadeMaxValue = findMax(this.teamData, this.goalsMadeHeader);
-        // let goalsConcededMaxValue = findMax(this.teamData, this.goalsConcededHeader);
-        // let deltaGoalsMaxValue = findMax(this.teamData, this.deltaGamesHeader);
-
-        // console.log("finding the min value for the scale");
-        // let goalsMadeMinValue = findMin(this.teamData, this.goalsMadeHeader);
-        // let goalsConcededMinValue = findMin(this.teamData, this.goalsConcededHeader);
-        // let deltaGoalsMinValue = findMin(this.teamData, this.deltaGamesHeader);
-
-        
-        // console.log("The max value for goals conceded");
-        // console.log(goalsConcededMaxValue);
-
-        // console.log("The min value for goals conceded");
-        // console.log(goalsConcededMinValue);
-
-        // console.log("The max value for goals made");
-        // console.log(goalsMadeMaxValue);
-
-        // console.log("The min value for goals made");
-        // console.log(goalsMadeMinValue);
-
-        // console.log("The max value for delta goals");
-        // console.log(deltaGoalsMaxValue);
-
-        // console.log("The min value for delta goals");
-        // console.log(deltaGoalsMinValue);
-        
+                                .range(['#cb181d', '#034e7b']);        
         // Create the axes
         
         //add GoalAxis to header of col 1.
