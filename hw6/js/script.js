7762ba9f1a87c63655ebcd072a84dfc860453a5b
Pranav Rajan <rajan.pranav320@gmail.com>
2019-10-16 23:27:40 -0600

    set up an svg and started messing around with the text layout

--- hw6/js/script.js
+++ hw6/js/script.js
@@ -3,6 +3,23 @@
  */
 d3.json("/data/words.json").then(data => {
   //console.log(data);
-
   let swarmChart = new SWARMCHART(data);
 });
+
+/**
+ * Set up the paragraph containing the text for the visualization
+ */
+let visInfo = d3.select("#header-wrap");
+let visText = visInfo.append("text")
+
+
+/**
+ * Set up the SVG canvas for the swarm chart
+ */
+ let chartView = d3.select("#chartView");
+ let chartSVG = chartView.append("svg");
+ chartSVG.attr("width", 1000)
+              .attr("height", 600)
+              .attr("id", "chartSVG");
+ 
+
