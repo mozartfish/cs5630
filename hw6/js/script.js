3aba714cd997d2cd7fa65d111ddaa3699186f56c
Pranav Rajan <rajan.pranav320@gmail.com>
2019-10-17 11:30:29 -0600

    wrote helper functions that calculate the max and min values for properties. Also calculated the max and min values for the scale for the svg

--- hw6/js/script.js
+++ hw6/js/script.js
@@ -1,23 +1,22 @@
 /**
+ * Set up the SVG canvas for the swarm chart
+ */
+let chartView = d3.select("#chartView");
+let chartSVG = chartView.append("svg");
+chartSVG.attr("width", 900)
+             .attr("height", 800)
+             .attr("id", "chartSVG");
+
+
+/**
  * Loads in the information for the visualization from words.json
  */
 d3.json("/data/words.json").then(data => {
   //console.log(data);
   let swarmChart = new SWARMCHART(data);
+  swarmChart.createChart();
 });
 
-/**
- * Set up the paragraph containing the text for the visualization
- */
-let visInfo = d3.select("#header-wrap");
-let visText = visInfo.append("text")
 
 
-/**
- * Set up the SVG canvas for the swarm chart
- */
- let chartView = d3.select("#chartView");
- let chartSVG = chartView.append("svg");
- chartSVG.attr("width", 900)
-              .attr("height", 800)
-              .attr("id", "chartSVG");
+
--- hw6/js/swarmchart.js
+++ hw6/js/swarmchart.js
@@ -1,3 +1,10 @@
+/**
+ * Some constants for properties that will be used a lot
+ */
+const position = "position";
+const sourceX = "sourceX";
+const sourceY = "sourceY";
+
 class SWARMCHART{
     /**
      * Create a swarmChart object
@@ -9,12 +16,41 @@ class SWARMCHART{
         this.chartData = politicalData;
         console.log("the data set is", politicalData);
 
+
         /**
-         * Set up the scales
+         * Scales
          */
         this.scalePoliticalScale = null;
     }
     createChart() {
+        console.log("entered the create chart function");
+        // helper function for finding the min value
+        function findMinValue(data, attribute)
+        {
+            let minValueList =[];
+            data.forEach(element => {
+                let value = element[attribute];
+                minValueList.push(value);
+            });
+            console.log("The min value list is", minValueList);
+            let minValue = d3.min(minValueList);
+            console.log("The min value is", minValue);
+        }
+        // helper function for finding the max value
+        function findMaxValue(data, attribute)
+        {
+            let maxValueList = [];
+            data.forEach(element => {
+                let value = element[attribute];
+                maxValueList.push(value);
+            });
+            console.log("The max value list is", maxValueList);
+            let maxValue = d3.max(maxValueList);
+            console.log("The max value is", maxValue); 
+        }
+        findMinValue(this.chartData, position);
+        findMaxValue(this.chartData, position);
+
         
     }
 
