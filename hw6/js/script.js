22c0f720ffb8fe017d7213ab79e1ad28abfba64b
Pranav Rajan <rajan.pranav320@gmail.com>
2019-10-25 13:35:59 -0600

    set up some of the basic variables for the table class and added in the access data function for figuring out the different categoriesf

--- hw6/js/script.js
+++ hw6/js/script.js
@@ -6,10 +6,11 @@
  * Load the data call the various scripts to generate the visualization
  */
 d3.json("/data/words.json").then(data => {
-  //   console.log("The data", data);
-
+  console.log("The data", data);
   // Create new Chart Object
   let chart = new Chart(data);
   chart.createChart();
   chart.drawChart();
+  // Create new Table Object
+  let table = new Table(data);
 });
--- hw6/js/table.js
+++ hw6/js/table.js
@@ -1,3 +1,51 @@
 /**
  * This script defines the table and its functionality as specified by the README
  */
+class Table{
+
+    /**
+     * Constructor for the table object
+     * @param {} politicalData - the data for the project
+     */
+    constructor(politicalData)
+    {
+        /**
+         * Instance variable for storing the table data
+         */
+        this.tableData = politicalData;
+
+        /**
+         * Instance variable for storing the table headers
+         */
+        this.tableHeaders = ["Phrase", "Frequency", "Percentage", "Total"];
+
+        /**
+         * Object for defining how to size the svgs in the table cells
+         */
+        this.cell = {"width": 70, "height": 20, "buffer":15};
+
+        /**
+         * Instance variable for defining the size of the bars for the table
+         */
+        this.bar = {"height": 20};
+    }
+
+      /**
+   * Function for gathering the different properties of the data without duplicates
+   * @param {*} data - the project data
+   * @param {*} attribute - a particular property of the data
+   */
+  accessData(data, attribute) {
+    let attributeValueSet = new Set();
+    let attributeValueList = [];
+    data.forEach(element => {
+      let value = element[attribute];
+      attributeValueSet.add(value);
+    });
+    attributeValueSet.forEach(element => {
+      attributeValueList.push(element);
+    });
+    return attributeValueList;
+  }
+
+}
